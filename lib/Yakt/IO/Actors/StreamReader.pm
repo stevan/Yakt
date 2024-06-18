0364275c110bf563b66555fe6a5d92801225bafd
Stevan Little <stevan.little@gmail.com>
2024-06-18 21:14:34 +0200

    official stream reader stuff and stuff

---
+++ lib/Yakt/IO/Actors/StreamReader.pm
@@ -0,0 +1,93 @@
+#!perl
+
+use v5.40;
+use experimental qw[ class ];
+
+use Yakt::Streams;
+
+use Yakt::System::Signals::IO;
+
+use Yakt::System::IO::Selector::Stream;
+use Yakt::System::IO::Reader::LineBuffered;
+
+class Yakt::IO::Actors::StreamReader :isa(Yakt::Actor) {
+    use Yakt::Logging;
+
+    field $fh :param;
+
+    field $subscriber;
+    field $watcher;
+    field $buffer;
+
+    ADJUST {
+        $buffer = Yakt::System::IO::Reader::LineBuffered->new( buffer_size => 128 );
+    }
+
+    # ... Signals
+
+    method subscribe :Receive(Yakt::Streams::Subscribe) ($context, $message) {
+        $context->logger->log(DEBUG, "Subscribe called" ) if DEBUG;
+
+        $subscriber = $message->subscriber;
+        $subscriber->send( Yakt::Streams::OnSubscribe->new( sender => $context->self ) );
+
+        $context->logger->log(INFO, "Started, creating watcher for fh($fh) ... ") if INFO;
+        $watcher = Yakt::System::IO::Selector::Stream->new( ref => $context->self, fh => $fh );
+        $context->system->io->add_selector( $watcher );
+        $watcher->is_reading = true;
+    }
+
+    method unsubscribe :Receive(Yakt::Streams::Unsubscribe) ($context, $message) {
+        $context->logger->log(DEBUG, "Unsubscribe called" ) if DEBUG;
+
+        $context->logger->log(INFO, "... removing watcher for fh($fh) ... ") if INFO;
+        $context->system->io->remove_selector( $watcher );
+        $watcher = undef;
+
+        $subscriber->send( Yakt::Streams::OnUnsubscribe->new );
+        $subscriber = undef;
+
+        $context->stop;
+    }
+
+    method on_stopping :Signal(Yakt::System::Signals::Stopping) ($context, $signal) {
+        if ($watcher) {
+            $context->logger->log(INFO, "Stopping, removing watcher for fh($fh) ... ") if INFO;
+            $context->system->io->remove_selector( $watcher );
+        }
+
+        if ($subscriber) {
+            $context->logger->log(INFO, "Stopping, removing subscriber, this is an error... ") if INFO;
+            $subscriber->send( Yakt::Streams::OnError->new( error => 'Stream stopped early for some reason???') );
+        }
+    }
+
+
+    # ... IO Signals
+
+    method can_read :Signal(Yakt::System::Signals::IO::CanRead) ($context, $signal) {
+        if ($buffer->read($fh)) {
+            $context->logger->log(INFO, "Read bytes from fh($fh)") if INFO;
+            if (my @lines = $buffer->flush_buffer) {
+                $context->logger->log(INFO, "Got ".(scalar @lines)." lines reading fh($fh)") if INFO;
+                $subscriber->send(Yakt::Streams::OnNext->new( sender => $context->self, value => $_ ))
+                    foreach @lines;
+            }
+            else {
+                $context->logger->log(INFO, "No lines read from fh($fh)") if INFO;
+            }
+        }
+
+        if (my $e = $buffer->got_error) {
+            $context->logger->log(ERROR, "Got error($e) reading fh($fh)") if ERROR;
+            $subscriber->send(Yakt::Streams::OnError->new( sender => $context->self, error => $e ));
+            $watcher->is_reading = false;
+        }
+
+        if ($buffer->got_eof) {
+            $context->logger->log(INFO, "Got EOF reading fh($fh)") if INFO;
+            $subscriber->send(Yakt::Streams::OnCompleted->new( sender => $context->self ));
+            $watcher->is_reading = false;
+        }
+    }
+}
---
+++ t/401-flow-and-io.t
@@ -0,0 +1,73 @@
+#!perl
+
+use v5.40;
+use experimental qw[ class ];
+
+use Test::More;
+
+use ok 'Yakt::System';
+
+use Yakt::Streams;
+
+use Yakt::IO::Actors::StreamReader;
+use Yakt::Streams::Actors::Operator::Map;
+
+class MyObserver :isa(Yakt::Streams::Actors::Observer) {
+    use Yakt::Logging;
+
+    our @RESULTS;
+    our $COMPLETED = 0;
+    our $ERROR;
+
+    method on_next ($context, $message) {
+        $context->logger->log(INFO, "->OnNext called" ) if INFO;
+        push @RESULTS => $message->value;
+    }
+
+    method on_completed ($context, $message) {
+        $context->logger->log(INFO, "->OnCompleted called" ) if INFO;
+        $message->sender->send( Yakt::Streams::Unsubscribe->new( subscriber => $context->self ) );
+        $COMPLETED++;
+        $context->logger->log(INFO, (join "\n" => @RESULTS) ) if INFO;
+    }
+
+    method on_error ($context, $message) {
+        $context->logger->log(INFO, "->OnError called" ) if INFO;
+        $ERROR = $message->error;
+    }
+
+    method on_unsubscribe ($context, $message) {
+        $context->logger->log(INFO, "->OnUnsubscribe called" ) if INFO;
+        $context->stop;
+    }
+
+}
+
+
+my $sys = Yakt::System->new->init(sub ($context) {
+
+    my $fh = IO::File->new;
+    $fh->open(__FILE__, 'r');
+
+    my $s = $context->spawn( Yakt::Props->new( class => Yakt::IO::Actors::StreamReader::, args => { fh => $fh }));
+    my $o = $context->spawn( Yakt::Props->new( class => MyObserver:: ));
+
+    my $m = $context->spawn( Yakt::Props->new( class => Yakt::Streams::Actors::Operator::Map::, args => {
+        f => sub ($line) {
+            state $line_no = 0;
+            sprintf '%4d : %s', ++$line_no, $line
+        }
+    }));
+
+    $s->send( Yakt::Streams::Subscribe->new( subscriber => $m ) );
+    $m->send( Yakt::Streams::Subscribe->new( subscriber => $o ) );
+
+});
+
+$sys->loop_until_done;
+
+like($MyObserver::RESULTS[-1], qr/# THE END$/, '... got the expected last line for fh');
+
+done_testing;
+
+# THE END
